b7c7445003fd59ed8edaf22c20fadeecca9c8b6c
Curtis <curtischung1227@gmail.com>
2026-06-18 14:18:56 -0400

    partner b task 1 finished

--- week3_day4/partner_b/Task.java
+++ week3_day4/partner_b/Task.java
@@ -6,11 +6,32 @@ package week3_day4.partner_b;
  */
 public class Task implements Comparable<Task> {
     // TODO fields priority, description
+    private int priority;
+    private String description;
 
     // TODO ctor, getters, toString
 
+    public Task(int priority, String description){
+        this.priority = priority;
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    //lower priority number = higher priority
     @Override
-    public int compareTo(Task o) {
-        throw new UnsupportedOperationException("TODO");
+    public int compareTo(Task other) {
+        return Integer.compare(this.priority, other.priority);
+    }
+
+    @Override
+    public String toString(){
+        return "Priority: " + priority + ", Description:" + description;
     }
 }
--- week3_day4/partner_b/TaskQueueApp.java
+++ week3_day4/partner_b/TaskQueueApp.java
@@ -8,5 +8,19 @@ public class TaskQueueApp {
     public static void main(String[] args) {
         Queue<Task> q = new PriorityQueue<>();
         // TODO: offer tasks out of order, poll and print, peek demo
+        
+        //q.offer(new Task(3, "wake up"));
+        q.offer(new Task(1, "run"));
+        q.offer(new Task(5, "jump"));
+        q.offer(new Task(2, "crawl"));
+
+        System.out.println("Peek:");
+        System.out.println(q.peek());
+
+        System.out.println("\n Poll order:");
+
+        while(!q.isEmpty()){
+            System.out.println(q.poll());
+        }
     }
 }
